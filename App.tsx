5d9146a25f4ffcad5a8631a8d09531b83c6e25fb
tugba-koc <koctug@itu.edu.tr>
2023-03-17 19:18:49 +0300

    first commit

---
+++ App.tsx
@@ -0,0 +1,23 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ *
+ * @format
+ */
+
+import React from 'react';
+import {SafeAreaView, ScrollView, Text, View} from 'react-native';
+
+function App(): JSX.Element {
+  return (
+    <SafeAreaView>
+      <ScrollView>
+        <View>
+          <Text>test</Text>
+        </View>
+      </ScrollView>
+    </SafeAreaView>
+  );
+}
+
+export default App;
