6693f86dcbbfc07c5611ea9ed28319445caf80a2
tugba-koc <koctug@itu.edu.tr>
2023-03-20 18:35:13 +0300

    feat: add react navigation

---
+++ routes/index.tsx
@@ -0,0 +1,28 @@
+import React from 'react';
+import Home from '../screens/Home/Home';
+import {NavigationContainer} from '@react-navigation/native';
+import {createNativeStackNavigator} from '@react-navigation/native-stack';
+import Detail from '../screens/Detail/Detail';
+
+const Stack = createNativeStackNavigator();
+
+const Routes = () => {
+  return (
+    <NavigationContainer>
+      <Stack.Navigator>
+        <Stack.Screen
+          options={{headerShown: false}}
+          name="Home"
+          component={Home}
+        />
+        <Stack.Screen
+          options={{headerShown: false}}
+          name="Detail"
+          component={Detail}
+        />
+      </Stack.Navigator>
+    </NavigationContainer>
+  );
+};
+
+export default Routes;
